88ba5c41fe940fa1d6b8888b2967f9beae5ff367
Kylie Merz <kmerz@ou.edu>
2021-08-25 11:35:25 -0500

    Finished loops 2

---
+++ 8-25-2021/8-25 Loops 2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _8_25_Loops_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double sum = 0;
+            int examCount = 0;
+            do
+            {
+                examCount++;
+                Console.WriteLine($"Enter your score (as a percent) for Exam {examCount.ToString("N0")} (e.g. 90% = .9");
+                sum += Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("do you have another exam score to enter? yes or no >>");
+            } while (Console.ReadLine().ToLower() == "yes");
+
+            Console.WriteLine($"Your average exam score is {(sum / examCount).ToString("P")}");
+        }
+    }
+}
