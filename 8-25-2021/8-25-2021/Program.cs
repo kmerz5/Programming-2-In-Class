7f1c76ca46a1bbafc3a96b55e4a2744b02d8dc84
Kylie Merz <kmerz@ou.edu>
2021-08-25 10:44:06 -0500

    Created in class

---
+++ 8-25-2021/8-25-2021/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _8_25_2021
+{
+    class Program
+    {
+        const double COG_COST = 79.99;
+        const double GEAR_COST = 250.00;
+        const double FULL_MARKUP_PERCENT = .15;
+        const double DISCOUNT_MARKUP_PERCENT = .125;
+
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
