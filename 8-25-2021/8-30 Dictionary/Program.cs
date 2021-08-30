1b484f52141f8f6f4672afc05da9aebf52b8f439
Kylie Merz <kmerz@ou.edu>
2021-08-30 11:46:11 -0500

    Dictionaries

---
+++ 8-25-2021/8-30 Dictionary/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _8_30_Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, double> fruits =new Dictionary<string, double>();
+            fruits.Add("Apples", 0.99);
+            //Add the other fruits
+
+            Console.WriteLine("What fruit do you8 want a price of? >>");
+            string fruit = Console.ReadLine();
+
+            if (fruits.ContainsKey(fruit) == true)
+            {
+                Console.WriteLine($"{fruit}'s cost {fruits[fruit]}");
+            }
+            else
+            {
+                Console.WriteLine($"Sorry we do not carry {fruit}'s");
+            }
+
+        }
+    }
+}
