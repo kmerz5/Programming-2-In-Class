12989f6ab52d65b9c53452ae142a83e07252be9c
Kylie Merz <kmerz@ou.edu>
2021-08-30 11:21:04 -0500

    Did Arrays 1

---
+++ 8-25-2021/8-30-21 Collections/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace _8_30_21_Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Arrays 1
+            string[] fruits = new string[5];
+            fruits[0] = "Apples";
+            fruits[1] = "Oranges";
+            fruits[2] = "Bananas";
+            fruits[3] = "Grapes";
+            fruits[4] = "Blueberries";
+
+            double[] prices = { .99, 0.50, 0.50, 2.99, 1.99 };
+
+            Console.WriteLine("What fruit would you like to find the price of? >>");
+            string fruit = Console.ReadLine();
+
+            int indexOfFruit = -1; //-1 because I could let them define the fruit but we will never have the value of -1
+
+            for (int i = 0; i < fruits.Length; i++)
+            {
+                if (fruits[i] == fruit)
+                {
+                    indexOfFruit = i;
+                    break;
+                }
+            }
+
+            if(indexOfFruit >= 0)
+            {
+                Console.WriteLine($"{fruit}'s cost {prices[indexOfFruit].ToString("C2")}");
+            }
+            else
+            {
+                Console.WriteLine($"Sorry, we do not carry {fruit}'s");
+            }
+
+        }
+    }
+}
