0fe990c73aeeea67ed896f8ce4332e3a5874de70
Kylie Merz <kmerz@ou.edu>
2021-08-30 11:32:08 -0500

    Arrays 2

---
+++ 8-25-2021/8-30 Arrays 2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _8_30_Arrays_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] lowercaseLetters = { 'a', 'b', 'c', 'd' };
+            char[] uppercaseLetters = { 'A', 'B', 'C', 'D' };
+
+            string firstName = "" + uppercaseLetters[0] + lowercaseLetters[3];
+            firstName = uppercaseLetters[0].ToString() + lowercaseLetters[3].ToString();
+
+            Console.WriteLine(firstName);
+        }
+    }
+}
