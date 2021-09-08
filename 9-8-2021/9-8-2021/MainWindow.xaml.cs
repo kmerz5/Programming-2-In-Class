970922e6b52bec5855c9ca7b92445d295ffde7ce
Kylie Merz <kmerz@ou.edu>
2021-09-08 11:32:53 -0500

    Finished participation

---
+++ 9-8-2021/9-8-2021/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _9_8_2021
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            
+        }
+
+        private void btn_enter_Click(object sender, RoutedEventArgs e)
+        {
+            string word = txt_AnyWord.Text;
+
+            char[] chars = new char[word.Length - 1];
+            for (int i = 0; i < chars.Length - 1; i++)
+            {
+                chars[i] = word[i];
+            }
+
+            for (int i = 0; i < chars.Length; i++)
+            {
+                if (chars[i] == 'a' | chars[i] == 'e' | chars[i] == 'i' | chars[i] == 'o' | chars[i] == 'u')
+                {
+                    lbx_vowels.Items.Add(chars[i]);
+
+                }
+                else
+                {
+                    lbx_Consonants.Items.Add(chars[i]);
+
+                }
+                
+
+            }
+
+        }
+    }
+}
