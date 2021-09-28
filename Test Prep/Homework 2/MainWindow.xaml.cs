77304b318f3ada9a7ce03b85ec5e364aef6919b6
Kylie Merz <kmerz@ou.edu>
2021-09-28 12:01:10 -0500

    Added the instructions

--- Test Prep/Homework 2/MainWindow.xaml.cs
+++ Test Prep/Homework 2/MainWindow.xaml.cs
@@ -27,7 +27,17 @@ namespace Homework_2
 
         private void btn_submitinfo_Click(object sender, RoutedEventArgs e)
         {
+            /*After they have input all this information and click a button to submit their information, validate 
+             * that all information provided by the user meets the data type associated with it (e.g. ints should be
+             * whole numbers, strings should not be empty, etc.). If any of the information is invalid, do not add the
+             * student to the handout and notify the user of ALL incorrect information, in whatever way you think is
+             * best for the user.*/
 
-        }
-    }
+
+            /*Once a student has successfully submitted their information for the handout, add their important 
+             * information to a ListBox. When added to the ListBox, it should show the object as follows: “FirstName 
+             * LastName, Major, Special Distinction.” (For this homework, lets assume that all students only graduate 
+             * with 1 major.) Make sure to add plenty of style! This is a graduation handout, after all.*/
+}
+}
 }
