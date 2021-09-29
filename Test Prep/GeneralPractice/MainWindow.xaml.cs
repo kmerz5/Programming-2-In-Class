a958676027b79db7e66beca6e3d2d3ad6c2cc28b
Kylie Merz <kmerz@ou.edu>
2021-09-28 22:36:10 -0500

    did vowels and consonants participation

---
+++ Test Prep/GeneralPractice/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GeneralPractice
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            /*Know how to read in a file
+             Process that file into objects - may or may not be given a UML
+            Be able to filter through this data to only show certain objects
+            be able to clean up the data before you can convert it to a certain object*/
+
+            /*SKILLS TO KNOW
+             creating a GUI
+            using controls (button, listbox, label, textbox, checkbox, radiobutton)
+            Adding events to controls (mousedoubleclick, mouseenter, click)
+            Reading ina  file (if you have to use the Open File Dialog, code will be given)
+            Reading a UML and reating a class appropriately from it
+            Getting data from the user using different controls (textbox etc.)
+            all 3013 material (conditionals, loops, string manipulation, etc.)*/
+        }
+    }
+}
---
+++ Test Prep/Vowels and Consonants (2)/MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Vowels_and_Consonants__2_
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            
+            InitializeComponent();
+            
+            /*Have a textbox that allows the user to type any word -----DONE----
+             There should be an "Enter" button that the user can click once they have input their word ----DONE----
+            There should be two list boxes  so that when a user clicks the button, the application will sort all vowels into one list box and all consonants into another list box ----DONE----
+            Hint: Look @ the Items property
+
+            The textbox should clear once the user has clicked the "Enter" button ----DONE----
+            Both list boxes should clear when the user enters a different word into the textbox*/
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            lbx_vowels.Items.Clear();
+            lbx_consonants.Items.Clear();
+            string word = txt_word.Text;
+            txt_word.Clear();
+
+
+            for (int i = 0; i < word.Length; i++)
+            {
+                char letter = word[i];
+                if (letter == 'a' || letter == 'e' || letter == 'i' || letter == 'o' || letter == 'u')
+                {
+                    lbx_vowels.Items.Add(letter);
+
+                }
+                else
+                {
+                    lbx_consonants.Items.Add(letter);
+                }
+                
+
+
+            }
+
+            
+        }
+    }
+}
