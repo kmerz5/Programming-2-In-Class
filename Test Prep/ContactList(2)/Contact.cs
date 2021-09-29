bc4fb6bc8784329ea076344fc5fe54bcdca3262b
Kylie Merz <kmerz@ou.edu>
2021-09-29 07:03:52 -0500

    started classes (attempt 2)

---
+++ Test Prep/ContactList(2)/Contact.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ContactList_2_
+{
+    public class Contact
+    {
+        public int Id {get; set;}
+        public string FirstName {get; set;}
+        public string LastName {get; set;}
+        public string Email {get; set;}
+        public string Photo { get; set; }
+
+
+
+        public Contact()
+        {
+            Id = 0;
+            FirstName = string.Empty;
+            LastName = string.Empty;
+            Email = string.Empty;
+            Photo = string.Empty;
+        }
+    }
+}
---
+++ Test Prep/ContactList(2)/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ContactList_2_
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            /*You are challenged to create a WPF Application so that you can load in your contacts (see file below).
+             * 
+             * Your application should read in the data ONCE and store it all in a ListBox 
+             * where the user can select a contact from the ListBox and press a button to see the details about the contact.  
+             * You should have labels and textboxes to accurately describe the data as well as the values, do NOT worry about showing the Id as that is just for storage purposes.  
+             * Although you will put the values in the Textbox's, the user should NOT be able to change the values (hint: this is a property we talked about on the control).  
+             * When the contact is in your ListBox, it should be displayed in the format : "{Lastname}, {Firstname}"
+
+            Do not use a textbox to show the Photo field, instead show the actual image of the contact.*/
+
+            string[] lines = File.ReadAllLines("contacts.txt");
+            for (int i = 1; i < lines.Length; i++)
+            {
+                string line = lines[i];
+                string[] pieces = line.Split('|');
+
+                string id = pieces[0];
+                string firstname = pieces[1];
+                string lastname = pieces[2];
+                string email = pieces[3];
+                string photo = pieces[4];
+
+                string output = $"{lastname}, {firstname}";
+                lbx_contacts.Items.Add(output);
+
+            }
+        }
+    }
+}
