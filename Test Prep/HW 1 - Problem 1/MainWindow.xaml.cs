7f35a15bffecb9a54131f77064f5ffb10769dcb3
Kylie Merz <kmerz@ou.edu>
2021-09-28 21:01:14 -0500

    completed the first part of homework 3

---
+++ Test Prep/HW 1 - Problem 1/MainWindow.xaml.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HW_1___Problem_1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private List<TVShow> TVShows = new List<TVShow>();
+        private char[] CharactersToTrim = { ' ', '"' };
+        public MainWindow()
+        {
+            InitializeComponent();
+            /*In Problem 1, you will read in the TV Show Dataset provided and create a WPF application to filter the data based on various combo boxes on your application. You will create three combo boxes on 
+             * your WPF application – one to display TV age ratings (rated), one for the TV show's country, and one for the TV show’s language.  Only display unique values in the combo box for filtering (e.g. 
+             * values should not repeat).*/
+
+            var lines = File.ReadAllLines("TV Show Data.txt").Skip(1);
+            //need to go through each line of the data - then storing it in the TVShows list
+            //foreach(type new variable in collection)
+            foreach (var line in lines)
+            {
+                TVShows.Add(new TVShow(line));
+
+            }
+
+            PopulateListBox(TVShows);
+            PopulateRatingFilter();
+            PopulateCountryFilter();
+            PopulateLanguageFilter();
+
+        }
+
+        private void PopulateLanguageFilter()
+        {
+            foreach (var show in TVShows)
+            {
+                string[] values = show.Language.Split(',');
+                foreach (var val in values)
+                {
+                    if (string.IsNullOrWhiteSpace(val))
+                    {
+                        continue;
+
+                    }
+
+                    string cleanedValue = val.Trim(CharactersToTrim);
+                    if (!cbx_language.Items.Contains(cleanedValue))
+                    {
+                        cbx_language.Items.Add(cleanedValue);
+
+                    }
+
+                }
+
+            }
+        }
+
+        private void PopulateCountryFilter()
+        {                               //we wanna go through the list
+            foreach (var show in TVShows)
+            {
+                var values = show.Country.Split(',');
+                //we need to split on the , because there are multiple countries in one line
+
+
+                foreach (var val in values)
+                {
+                    if (string.IsNullOrWhiteSpace(val))
+                    {
+                        continue;
+                    }
+
+                    string cleanedValue = val.Trim(CharactersToTrim);
+                    if (!cbx_country.Items.Contains(cleanedValue))
+                    {
+                        cbx_country.Items.Add(cleanedValue);
+
+                    }
+
+                }
+
+            }
+        }
+
+        private void PopulateRatingFilter()
+        {
+            //Wanting to populate the dropdown with all the possible ratings, without repeating
+            foreach (var show in TVShows)
+            {
+                if (string.IsNullOrWhiteSpace(show.Rated))
+                {
+                    //basically saying that if the rating is null or white space to keep filtering through all the lines
+                    continue;
+                }
+
+                //This basically trims the value of anything and then if the combo box doesnt already contain the rated value then it will add it, therefore avoiding repeats (this is what the ! is for)
+                string cleanedValue = show.Rated.Trim();//trimming the value of any unwanted punctuation (the trim statement is at the top)
+                if (!cbx_rated.Items.Contains(cleanedValue))
+                {
+                    cbx_rated.Items.Add(cleanedValue);
+
+                }
+
+            }
+        }
+
+        private void PopulateListBox(List<TVShow> tVShows)
+        {                                           //new variable that it created for us
+            //We want to populate the listbox on our xaml with all of the information about the movies
+            foreach (var show in tVShows)
+            {                       //using that new variable
+                    lbx_output.Items.Add(show);
+            }
+        }
+    }
+}
---
+++ Test Prep/HW 1 - Problem 1/TVShow.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HW_1___Problem_1
+{
+   public class TVShow
+    {
+        public string Actors { get; set; }
+        public string Awards { get; set; }
+        public string Country { get; set; }
+        public string Director { get; set; }
+        public string Genre { get; set; }
+        public string Language { get; set; }
+        public string Plot { get; set; }
+        public string Poster { get; set; }
+        public string Rated { get; set; }
+        public string Released { get; set; }
+        public string RuntimeInMinutes { get; set; }
+        public string Title { get; set; }
+        public string Type { get; set; }
+        public string Writer { get; set; }
+        public string Year { get; set; }
+        public string ImdbID { get; set; }
+        public string ImdbRating { get; set; }
+        public string ImdbVotes { get; set; }
+        public string TotalSeasons { get; set; }
+
+
+        public TVShow()
+        {
+            Actors = string.Empty;
+            Awards = string.Empty;
+            Country = string.Empty;
+            Director = string.Empty;
+            Genre = string.Empty;
+            Language = string.Empty;
+            Plot = string.Empty;
+            Poster = string.Empty;
+            Rated = string.Empty;
+            Released = string.Empty;
+            RuntimeInMinutes = string.Empty;
+            Title = string.Empty;
+            Type = string.Empty;
+            Writer = string.Empty;
+            Year = string.Empty;
+            ImdbID = string.Empty;
+            ImdbRating = string.Empty;
+            ImdbVotes = string.Empty;
+            TotalSeasons = string.Empty;
+        }
+
+        public TVShow(string line)
+        {
+            var pieces = line.Split('\t');
+            Actors = pieces[1];
+            Awards = pieces[2];
+            Country = pieces[3];
+            Director = pieces[4];
+            Genre = pieces[5];
+            Language = pieces[6];
+            Plot = pieces[7];
+            Poster = pieces[8];
+            Rated = pieces[9];
+            Released = pieces[10];
+            RuntimeInMinutes = pieces[11];
+            Title = pieces[12];
+            Type = pieces[13];
+            Writer = pieces[14];
+            Year = pieces[15];
+            ImdbID = pieces[16];
+            ImdbRating = pieces[17];
+            ImdbVotes = pieces[18];
+            TotalSeasons = pieces[19];
+        }
+
+        public override string ToString()
+        {
+            return $"{Title} is rated ({Rated}) created in {Country} in {Language} languages.";
+        }
+
+    }
+}
