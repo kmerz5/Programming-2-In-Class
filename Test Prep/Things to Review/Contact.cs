4ce18fe96863228bfafbec562a68a33058e24a92
Kylie Merz <kmerz@ou.edu>
2021-09-29 09:16:46 -0500

    generic review items

---
+++ Test Prep/Things to Review/Contact.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Things_to_Review
+{
+    public class Contact
+    {
+        public double Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Photo { get; set; }
+
+
+        public Contact()
+        {
+            Id = 0.0;
+            FirstName = string.Empty;
+            LastName = string.Empty;
+            Email = string.Empty;
+            Photo = string.Empty;
+        }
+
+        public Contact(double ID, string firstName, string lastName, string email, string photo)
+        {
+            Id = ID;
+            FirstName = firstName;
+            LastName = lastName;
+            Email = email;
+            Photo = photo;
+        }
+
+        public override string ToString()
+        {
+            return $"{LastName}, {FirstName}";
+        }
+
+
+    }
+}
---
+++ Test Prep/Things to Review/MainWindow.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Things_to_Review
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<Contact> contactS = new List<Contact>();
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            //How to create a list n(above)^^^^
+
+            //reading in a file and splitting that file
+            string[] filecomponents = File.ReadAllLines("contacts.txt");
+            for (int i = 1; i < filecomponents.Length; i++)
+            {
+                string file = filecomponents[i];
+                string[] pieces = file.Split('|');
+
+                string id = pieces[0];
+                double ID = Convert.ToDouble(id);
+                string firstname = pieces[1];
+                string lastname = pieces[2];
+                string email = pieces[3];
+                string photo = pieces[4];
+
+                //LOOK AT THIS LINE - HAVE TO CREATE A VARIABLE AND THEN CREATE THE NEW INSTANCE OF THE CLASS
+                Contact information = new Contact(ID, firstname, lastname, email, photo);
+                lbx_contacts.Items.Add(information);
+                contactS.Add(information);
+
+                
+
+            }
+
+            
+
+        }
+
+
+        //using the item selected in the listbox (contacts)
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Contact selected = (Contact)lbx_contacts.SelectedItem;
+            tb1.Text = selected.FirstName;
+            tb2.Text = selected.LastName;
+            tb3.Text = selected.Email;
+
+            var uri = new Uri(selected.Photo);
+            var img = new BitmapImage(uri);
+
+            img_picture.Source = img;
+
+        }
+    }
+}
