fe5c74b0c3d3372c779f426a70ccdc583cc4c2e7
Kylie Merz <kmerz@ou.edu>
2021-10-04 11:45:32 -0500

    finished

---
+++ (P)JSON - Chuck Norris Jokes/(P)JSON - Chuck Norris Jokes/Joke.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _P_JSON___Chuck_Norris_Jokes
+{
+    public class Joke
+    {
+        public List<string> categories { get; set; }
+        public string created_at { get; set; }
+        public string icon_url { get; set; }
+        public string id { get; set; }
+        public string updated_at { get; set; }
+        public string url { get; set; }
+        public string value { get; set; }
+
+
+        public override string ToString()
+        {
+            return $"{value}";
+        }
+
+    }
+}
---
+++ (P)JSON - Chuck Norris Jokes/(P)JSON - Chuck Norris Jokes/MainWindow.xaml.cs
@@ -0,0 +1,87 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _P_JSON___Chuck_Norris_Jokes
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            cbx_categories.Items.Add("All");
+            using (var client = new HttpClient())
+            {
+                string json = client.GetStringAsync("https://api.chucknorris.io/jokes/categories").Result;
+
+
+                List<string> categories = JsonConvert.DeserializeObject<List<string>>(json);
+
+                foreach (string cat in categories)
+                {
+                    cbx_categories.Items.Add(cat);
+
+                }
+                
+            }
+            
+        }
+
+        private void btn_joke_Click(object sender, RoutedEventArgs e)
+        {
+            string selected = cbx_categories.SelectedItem.ToString();
+
+            if (selected == "All")
+            {
+                using (var client = new HttpClient())
+                {
+                    string json = client.GetStringAsync("https://api.chucknorris.io/jokes/random").Result;
+
+                    Joke rand = JsonConvert.DeserializeObject<Joke>(json);
+                    txt_joke.Text = rand.value;
+
+                   
+
+                }
+
+
+            }
+            else
+            {
+                string cat = cbx_categories.Text;
+                using (var client = new HttpClient())
+                {
+                    string json = client.GetStringAsync("https://api.chucknorris.io/jokes/random?category=" + cat).Result;
+
+                    Joke rand1 = JsonConvert.DeserializeObject<Joke>(json);
+                    txt_joke.Text = rand1.value;
+
+
+
+                }
+
+            }
+            
+
+            
+
+        }
+    }
+}
