74dd991bf9bcd7d23a2c36db1cef74483b368238
Kylie Merz <kmerz@ou.edu>
2021-10-06 11:41:04 -0500

    Got the combo box to populate

---
+++ (P)JSON - Pokemon/(P)JSON - Pokemon/Data.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _P_JSON___Pokemon
+{
+    public class Data
+    {
+        public List<string> result { get; set; }
+        public string name { get; set; }
+        public string url { get; set; }
+
+
+        public override string ToString()
+        {
+            return $"{name}";
+        }
+    }
+}
---
+++ (P)JSON - Pokemon/(P)JSON - Pokemon/PokemonApi.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _P_JSON___Pokemon
+{
+
+    public class PokemonApi
+    {
+        public int count { get; set; }
+        public string next { get; set; }
+        public List<Data> results { get; set; }
+
+        
+
+    }
+}
