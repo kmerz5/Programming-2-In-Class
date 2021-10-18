385ba83dee2f7849c27c2e9626dbdc3fe5e88af9
Kylie Merz <kmerz@ou.edu>
2021-10-18 11:46:47 -0500

    cant get the combobox to populate weird error in the using statement

---
+++ (P) JSON - Serialization/(P) JSON - Serialization/Games.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _P__JSON___Serialization
+{
+    public class Games
+    {
+        //name,platform,release_date,summary,meta_score,user_review
+        public string name { get; set; }
+        public string platform { get; set; }
+        public string release_date { get; set; }
+        public string summary { get; set; }
+        public int meta_score { get; set; }
+        public double user_review { get; set; }
+
+        public Games()
+        {
+            name = string.Empty;
+            platform = string.Empty;
+            release_date = string.Empty;
+            summary = string.Empty;
+            meta_score = 0;
+            user_review = 0.0;
+        }
+
+        public Games(string gamename, string gameplatform, string gamerelease_date, string gamesummary, int gamemeta_score, double gameuser_review)
+        {
+            name = gamename;
+            platform = gameplatform;
+            release_date = gamerelease_date;
+            summary = gamesummary;
+            meta_score = gamemeta_score;
+            user_review = gameuser_review;
+
+        }
+
+        public override string ToString()
+        {
+            return $"{name}";
+        }
+    }
+}
--- (P) JSON - Serialization/(P) JSON - Serialization/MainWindow.xaml.cs
+++ (P) JSON - Serialization/(P) JSON - Serialization/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +21,45 @@ namespace _P__JSON___Serialization
     /// </summary>
     public partial class MainWindow : Window
     {
+        List<Games> games = new List<Games>();
+        List<string> platforms = new List<string>();
         public MainWindow()
         {
             InitializeComponent();
+
+            //populate the combo box with the platforms
+            string[] lines = File.ReadAllLines("all_games(1).csv");
+            Games game1 = new Games();
+            for (int i = 1; i < lines.Length; i++)
+            {
+                string line = lines[i];
+                string[] pieces = line.Split(',');
+
+
+                string name = pieces[0];
+                string platform = pieces[1];
+                string release_date = pieces[2];
+                string summary = pieces[3];
+                string meta_score = pieces[4];
+                int gamemeta_score = Convert.ToInt32(meta_score);
+                string user_review = pieces[5];
+                double gameuser_review = Convert.ToDouble(user_review);
+
+                Games information = new Games(name, platform, release_date, summary, gamemeta_score, gameuser_review);
+                games.Add(information);
+
+                //populate the combobox
+                    if (cbx_platform.Text.Contains(game1.platform) == false)
+                    {
+                        cbx_platform.Items.Add(game1.platform);
+
+                    }
+
+                
+                
+
+            }
+
         }
     }
 }
