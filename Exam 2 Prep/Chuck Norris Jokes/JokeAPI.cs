2872708b892b25a463fec9c90f6cf4fa85b13d5b
Kylie Merz <kmerz@ou.edu>
2021-10-26 15:03:23 -0500

    Added categories to the combo box

---
+++ Exam 2 Prep/Chuck Norris Jokes/JokeAPI.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chuck_Norris_Jokes
+{
+    public class JokeAPI
+    {
+        public List<JokeAPI> category {get; set;}
+
+        public string value { get; set; }
+
+    }
+}
--- Exam 2 Prep/Chuck Norris Jokes/MainWindow.xaml.cs
+++ Exam 2 Prep/Chuck Norris Jokes/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -24,12 +26,35 @@ namespace Chuck_Norris_Jokes
      * We are going to put a pretty background image (from the internets) for our form of Chuck Norris. 
      * We will allow the user to select any of the provided categories from the API via a ComboBox, they should also have an option of 'All'
      * (that will be the first item listed in the box)  which will just get a joke from any of the categories (hint: will remove that URL parameter).  
-     * Once they select an item from the ComboBox, the user will then press a button that will let you know that you should go get a quote from whatever they selected and then display it on the page.*/
+     * Once they select an item from the ComboBox, the user will then press a button that will let you know that you should go get a quote from whatever they selected and then display it on the page.
+     
+     
+     GUI
+     - background image DONE
+     - combox box to select the provided categories DONE
+     - button to display quote DONE
+     - Textbox to display quote DONE*/
     public partial class MainWindow : Window
     {
+       
         public MainWindow()
         {
             InitializeComponent();
+
+            using (var client = new HttpClient())
+            {
+                string json = client.GetStringAsync("https://api.chucknorris.io/jokes/categories").Result;
+
+                List<string> category = JsonConvert.DeserializeObject<List<string>>(json);
+
+                foreach (var cat in category)
+                {
+                    cbx_categories.Items.Add(cat);
+
+                }
+
+                
+            }
         }
     }
 }
