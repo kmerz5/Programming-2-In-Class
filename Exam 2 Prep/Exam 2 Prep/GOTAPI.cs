3125869454191fefd0364e0c68c738f561e3b3e8
Kylie Merz <kmerz@ou.edu>
2021-10-26 15:50:56 -0500

    Finished GOT Quote

---
+++ Exam 2 Prep/Exam 2 Prep/GOTAPI.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exam_2_Prep
+{
+    public class GOTAPI
+    {
+        public string quote { get; set; }
+        public string character { get; set; }
+
+
+        public override string ToString()
+        {
+            return $"{quote} - {character}";
+        }
+    }
+}
--- Exam 2 Prep/Exam 2 Prep/MainWindow.xaml.cs
+++ Exam 2 Prep/Exam 2 Prep/MainWindow.xaml.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -26,12 +29,45 @@ namespace Exam_2_Prep
     The random quote can be found by going to https://got-quotes.herokuapp.com/quotes (Links to an external site.)
 
     There should be a different button that when pressed (maybe named Export?), will save all of the jokes that the user got to a file on their computed named GOT_Quotes.json .  
-    So if the user randomly generated 15 random quotes, your file will have 15 quotes in it.*/
+    So if the user randomly generated 15 random quotes, your file will have 15 quotes in it.
+    
+     
+     GUI
+     - GOT image as the background DONE
+     - Button to get a random quote DONE
+     - textblock to show the quote and who said it DONE
+     - export button DONE*/
+
+  
     public partial class MainWindow : Window
     {
+        List<GOTAPI> Quotes = new List<GOTAPI>();
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void btn_getquote_Click(object sender, RoutedEventArgs e)
+        {
+            GOTAPI quote;
+            using (var client = new HttpClient())
+            {
+                string json = client.GetStringAsync("https://got-quotes.herokuapp.com/quotes").Result;
+                 quote = JsonConvert.DeserializeObject<GOTAPI>(json);
+
+                
+
+            }
+
+            txt_quote.Text = quote.quote;
+            txt_character.Text = quote.character;
+            Quotes.Add(quote);
+        }
+
+        private void btnExport_Click(object sender, RoutedEventArgs e)
+        {
+            string json = JsonConvert.SerializeObject(Quotes);
+            File.WriteAllText("GOT_Quotes.Json", json);
         }
     }
 }
