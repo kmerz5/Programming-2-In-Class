75d0c4741e914b4b39ba419c0a1db645d730218d
Kylie Merz <kmerz@ou.edu>
2021-10-26 17:34:13 -0500

    populated combo box with the platforms

---
+++ Exam 2 Prep/Serialization/Game.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Serialization
+{
+    public class Game
+    {
+
+        //name,platform,release_date,summary,meta_score,user_review
+
+        public string name { get; set; }
+        public string platform { get; set; }
+        public string release_date { get; set; }
+        public string summary { get; set; }
+        public double meta_score { get; set; }
+        public string user_review { get; set; }
+
+        public Game()
+        {
+            name = string.Empty;
+            platform = string.Empty;
+            release_date = string.Empty;
+            summary = string.Empty;
+            meta_score = 0.0;
+            user_review = string.Empty;
+        }
+
+        public Game(string Name, string Platform, string Release_Date, string Summary, double Meta_Score, string User_Review)
+        {
+            name = Name;
+            platform = Platform;
+            release_date = Release_Date;
+            summary = Summary;
+            meta_score = Meta_Score;
+            user_review = User_Review;
+        }
+
+    }
+}
--- Exam 2 Prep/Serialization/MainWindow.xaml.cs
+++ Exam 2 Prep/Serialization/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -22,17 +23,55 @@ namespace Serialization
 
     /*Create a GUI application to allow the user to look through, and filter, the all_games.csv  Download all_games.csvdata set. 
 
-    The user should be able to select the platform from a ComboBox (should be populated from the data and have unique values), All should be a valid option listed at the top as well.  
+    The user should be able to select the platform from a ComboBox (should be populated from the data and have unique values),
+    All should be a valid option listed at the top as well.  
     The application should load with all the games listed in a ListBox (Format: "{Name}"). 
 
     When the user selects a different value in the ComboBox, the dataset should update.  If the user double clicks any item in the ListBox, display the details of the game in a NEW WINDOW 
+    
+
     (Hint: Make a new Window (similar to making a new Class) and when the event is called, make a new instance of the Window you just created and pass in the selected game).  
-    There should be a button that when pressed, will take all of the objects inside of the ListBox and serialize them to JSON to a file where the application is being ran named "games.json"*/
+    There should be a button that when pressed, will take all of the objects inside of the ListBox and serialize them to JSON to a file where the application is being ran named "games.json"
+    
+     GUI
+     - Combo box for the platform
+     - Listbox with all the games*/
+
     public partial class MainWindow : Window
     {
+
+        List<Game> allgames = new List<Game>();
         public MainWindow()
         {
             InitializeComponent();
+
+            string[] lines = File.ReadAllLines("all_games (1).csv").Skip(1).ToArray();
+
+            foreach (var line in lines)
+            {
+                string[] pieces = line.Split(",");
+
+                string name = pieces[0];
+                
+                string platform = pieces[1];
+                string release_date = pieces[2];
+                string summary = pieces[3];
+                string meta_score = pieces[4];
+                double Meta_Score = Convert.ToDouble(meta_score);
+                string user_review = pieces[5];
+
+                Game games = new Game(name, platform, release_date, summary, Meta_Score, user_review);
+                allgames.Add(games);
+
+                if (cbx_platforms.Items.Contains(games.platform) == false)
+                {
+                    cbx_platforms.Items.Add(games.platform);
+
+                }
+               
+
+            }
+
         }
     }
 }
