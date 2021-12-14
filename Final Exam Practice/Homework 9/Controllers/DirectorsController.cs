1381329e15e9145653fe112876608df006f21663
Kylie Merz <kmerz@ou.edu>
2021-12-14 16:34:52 -0600

    Finished hw9

---
+++ Final Exam Practice/Homework 9/Controllers/DirectorsController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Homework_9.Models;
+
+namespace Homework_9.Controllers
+{
+    public class DirectorsController : Controller
+    {
+        private DB_128040_practiceEntities db = new DB_128040_practiceEntities();
+
+        // GET: Directors
+        public ActionResult Index()
+        {
+            return View(db.Directors.ToList());
+        }
+
+        // GET: Directors/Details/5
+        public ActionResult Info(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Director director = db.Directors.Find(id);
+            if (director == null)
+            {
+                return HttpNotFound();
+            }
+            return View(director);
+        }
+
+       
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Final Exam Practice/Homework 9/Controllers/TopMoviesController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Homework_9.Models;
+
+namespace Homework_9.Controllers
+{
+    public class TopMoviesController : Controller
+    {
+        private DB_128040_practiceEntities db = new DB_128040_practiceEntities();
+
+        // GET: TopMovies
+        public ActionResult Movies(int number=10)
+        {
+            var movies = db.Movies.Include(m => m.Director).OrderByDescending(x => x.gross).Take(number);
+            return View(movies.ToList());
+        }
+
+      
+
+        
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
